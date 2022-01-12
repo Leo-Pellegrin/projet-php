72674c14daaa0ac5494c875b3ea4d0312de9d7ae
Samaar Clément <77165371+ClementSamaar@users.noreply.github.com>
2022-01-12 16:09:03 +0100

    Changement nom de repertoire et modif du constructeur de la classe campagne (Clément)

--- Models/Campagne.php
+++ Models/Campagne.php
@@ -16,6 +16,7 @@ class Campagne
             echo 'L\'évenement ne peut être terminé avant d\'avoir commencé';
             unset($this->datedeb);
             unset($this->datefin);
+            exit();
         }
         else {
             $this->nom = $nom;
