3ab8b5e220be4991e1c3bc84ecf46531aadb8040
Samaar Clément <77165371+ClementSamaar@users.noreply.github.com>
2022-01-17 18:34:27 +0100

    Update viewLogin.php

--- Views/viewLogin.php
+++ Views/viewLogin.php
@@ -30,6 +30,7 @@
         </ol>
     </nav>
 </header>
+
     <?php
         if($errorMsg != null)
             echo '<h2 id="err_h2">'.$errorMsg.'</h2>';
@@ -40,6 +41,23 @@
                 <input type="text" name="identifiant" placeholder="Email / Username" required/><br/>
                 <input type="password" name="password" placeholder="Mot de passe" required/><br/>
 
+    <h2>Se connecter</h2>
+    <div class="form">
+        <form method="post">
+            <label>Identifiant</label>
+            <input name="identifiant" placeholder="Email / Username" type="text" required> <br>
+            <label>Mot de passe</label>
+            <input name="password" type="password" required> <br>
+            <input type="submit" style="margin-left: 22%;cursor: pointer;" name="login" value="Se connecter">
+        </form>
+    </div>
+
+<footer>
+    <a href=""><img src="../images/html5.png" alt="Valid XHTML5"></a>
+    <a class="css" href=""><img src="../images/css3.png" alt="Valid CSS3"></a>
+    <a href="mailto:projet.phpiutaix@gmail.com">Contactez nous par mail !</a>
+    <p>©e-event.io 2022</p>
+</footer>
             <input type="submit" style="margin-left: 36.5%;cursor: pointer;" name="login" value="Se connecter">
         </form>
 
