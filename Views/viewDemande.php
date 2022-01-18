e4206a5f0ade41d4e860d4bc6f0c2148e937fb15
Leo-Pellegrin <leo.pellegrin@etu.univ-amu.fr>
2022-01-18 22:41:43 +0100

    Identation + début correction W3C

--- Views/viewDemande.php
+++ Views/viewDemande.php
@@ -10,10 +10,10 @@
                 <h1>e-event.io</h1>
                 <?php if(isset($_SESSION['username'])){ ?>
                     <p>Bienvenue, <?= $_SESSION['username'] ?> !</p>
-                    <a href="deco"><input type="submit" value="Déconnexion"></a>
+                    <a href="deco" id="but_a">Déconnexion</a>
                 <?php }else{ ?>
-                    <a href="register"><input type="submit" value="S'inscrire"></a>
-                    <a href="login"><input type="submit" value="Se connecter"></a>
+                    <a href="register" id="but_a">S'inscrire</a>
+                    <a href="login" id="but_a">Se connecter</a>
                 <?php } ?>
             </div>
 
@@ -85,7 +85,7 @@
 
     <footer>
         <a href=""><img src="../images/html5.png" alt="Valid XHTML5"></a>
-        <a class="css" href=""><img src="../images/css3.png" alt="Valid CSS3"></a>
+        <a class="css" href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fprojetphp45.alwaysdata.net%2Fcss%2Fstyle.css&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"><img src="../images/css3.png" alt="Valid CSS3"></a>
         <a href="mailto:projet.phpiutaix@gmail.com">Contactez nous par mail !</a>
         <p>©e-event.io 2022</p>
     </footer>
