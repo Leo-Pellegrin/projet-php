ea941c41ff9a0006f1e8490d6160c0eeb1c4b13c
Leo-Pellegrin <leo.pellegrin@etu.univ-amu.fr>
2022-01-18 22:55:09 +0100

    W3C validé

--- Views/viewRegister.php
+++ Views/viewRegister.php
@@ -7,13 +7,15 @@
         <header>
             <div id="main_div">
                 <h1>e-event.io</h1>
-                <?php if(isset($_SESSION['username'])){ ?>
-                    <p>Bienvenue, <?= $_SESSION['username'] ?> !</p>
-                    <a href="deco" id="but_a">Déconnexion</a>
-                <?php }else{ ?>
-                    <a href="register" id="but_a">S'inscrire</a>
-                    <a href="login" id="but_a">Se connecter</a>
-                <?php } ?>
+                <div id="but_a">
+                    <?php if(isset($_SESSION['username'])){ ?>
+                        <p>Bienvenue, <?= $_SESSION['username'] ?> !</p>
+                        <a href="deco">Déconnexion</a>
+                    <?php }else{ ?>
+                        <a href="register">S'inscrire</a>
+                        <a href="login">Se connecter</a>
+                    <?php } ?>
+                </div>
             </div>
         </header>
 
@@ -47,7 +49,7 @@
 
 
         <footer>
-            <a href=""><img src="../images/html5.png" alt="Valid XHTML5"></a>
+            <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fprojetphp45.alwaysdata.net%2F"><img src="../images/html5.png" alt="Valid XHTML5"></a>
             <a class="css" href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fprojetphp45.alwaysdata.net%2Fcss%2Fstyle.css&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"><img src="../images/css3.png" alt="Valid CSS3"></a>
             <a href="mailto:projet.phpiutaix@gmail.com">Contactez nous par mail !</a>
             <p>©e-event.io 2022</p>
